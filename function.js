14c7f50b8a9b468b9a953ae52dbb62cf0cac4fa9
Dwi Yulianto <98dwiyulianto@gmail.com>
2020-10-17 16:48:20 +0700

    tugas 31 JS
    tugas untuk Niomic

---
+++ function.js
@@ -0,0 +1,11 @@
+// const tombol = document.getElementsByTagName('button')[0];
+
+// tombol.addEventListener('click', function() {
+//     console.log(`Tugas berhasil !!`);
+// })
+
+// atau
+
+function tombol() {
+    console.log('Tugas berhasil !!');
+}
